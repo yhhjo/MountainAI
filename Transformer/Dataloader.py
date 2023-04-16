8fc7890c3e162e80c6a97454a6bbd1a89975049c
yhhjo <ajb28@live.unc.edu>
2023-04-15 22:50:41 -0400

    Structural changes

--- Transformer/Dataloader.py
+++ Transformer/Dataloader.py
@@ -1,31 +1,38 @@
 import numpy as np
-import Sample
 
-def process_input(x)->np.array([]):
-    """Takes input of shape H_thermal and concatenates the normalized vectors (age, AHe, Aft, ZHe, Zft)
-    Output is shape d_input. 
-    """
-    x = np.array([*x])[1:,:]
-    std = np.std(x, axis=1, keepdims=True)
-    std[std==0] = .00001
-    x = ((x - np.mean(x, axis=1, keepdims=True)) / std).T
-    return x
+def process_input(dataset, normalize=True):
+    """Pulls inputs from dataset and returns an array (dataset_size, 10, 5) normalized across the 1st dimension"""
+    y = np.array([*dataset])[:,0]
+    y = np.array([np.array(z) for z in y])
 
-def process_output(y: Sample, d_output: int) -> np.array([]):
-    """y: Sample object. Returns a concatenated, padded array of size d_output"""
-    y = [y.duration, y.num_events, y.basement_cond, y.surface_hp, y.hp_depth, y.surf_hf, y.surf_temp, 
-    *y.exhumation_history.flatten(), *y.exhumation_rates]
-    y = np.pad(y, (0, d_output-len(y)), constant_values=0) #Pad
+    if normalize:
+        std = np.std(y, axis=1, keepdims=True)
+        mean = np.mean(y, axis=1, keepdims=True)
+        std[std==0] = .0001
+        y = (y - mean) / std
+    y = np.transpose(y, (0, 2, 1))
     return y
+    
+def process_output(dataset, d_output: int) -> np.array([]):
+    """Input: dataset. Returns the concatenated values of Sample object 0-padded to be size d_output"""
+    Y = np.array([*dataset])[:,1]
+    ret = np.zeros((Y.shape[0], d_output))
+    for i, y in enumerate(Y):
+        t = [y.duration, y.num_events, y.basement_cond, y.surface_hp, y.hp_depth, y.surf_hf, y.surf_temp, 
+        *y.exhumation_history.flatten(), *y.exhumation_rates]
+        t = np.pad(t, (0, d_output-len(t)), constant_values=0) #Pad
+        ret[i] = t
+    return ret
 
-def formattedLoader(d_input, d_output, dataset_path):
+def formattedLoader(d_output, dataset_path):
     """Returns data formatted specifically for transformer"""
     dataset = np.load(dataset_path,allow_pickle=True)
+    X = process_input(dataset)
+    Y = process_output(dataset, d_output)
     formatted_data = []
-    for x, y in dataset:
-        x = process_input(x)
-        y = process_output(y, d_output)
-        formatted_data.append((x,y))
+    for (x, y) in zip(X, Y):
+        formatted_data.append((x, y))
+
     return formatted_data
 
 def rawLoader(dataset_path: str)->np.array([]):
